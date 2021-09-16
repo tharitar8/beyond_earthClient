d31faf320a569f92cd271ec25962994ee5987635
tharitar8 <86535497+tharitar8@users.noreply.github.com>
2021-09-16 10:33:43 -0400

    make a state out of stock with disabled button

--- src/components/Products/ProductPage.js
+++ src/components/Products/ProductPage.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../Products/Rating'
 import products from '../../products'
 
 function ProductPage ({ match }) {
-  // look for id value
+// look for id value
   const product = products.find((p) => p._id === match.params.id)
   return (
     <div>
-      <Link to='/' className='btn btn-light may-3'>Return
+      <Link to='/' className='btn btn-light may-3'>
+            Back
       </Link>
       <Row>
         <Col md={6}>
@@ -32,14 +33,33 @@ function ProductPage ({ match }) {
 
           <ListGroup.Item>Detail: {product.description}</ListGroup.Item>
         </Col>
+
         <Col md={3}>
           <Card>
             <ListGroup variant='flush'>
               <ListGroup.Item>
                 <Row>
-                  <Col> Price: ${product.price}</Col>
-                  <Col></Col>
+                  <Col>
+                                    Price: <strong>${product.price}</strong>
+                  </Col>
                 </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>
+                                    Status :{' '}
+                    {product.countInStock > 0 ? 'In stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  className='btn btn-dark'
+                  disabled= {product.countInStock === 0}
+                >  Add to cart
+                </Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
