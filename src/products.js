d68ea7ffc99cd226b1bc140b9dea953c40f90ea9
tharitar8 <86535497+tharitar8@users.noreply.github.com>
2021-09-15 20:49:12 -0400

    attempted add ratings stars

--- src/products.js
+++ src/products.js
@@ -2,9 +2,8 @@ const products = [
   {
     _id: '1',
     name: 'Mars Ticket',
-    image: '../images/mars.png',
-    description:
-                'getaway from this earth',
+    image: 'https://i.imgur.com/6FSlQaR.png',
+    description: 'getaway from this earth',
     company: 'spaceX',
     category: 'Mars',
     price: 50000,
@@ -15,9 +14,8 @@ const products = [
   {
     _id: '2',
     name: 'Moon Ticket',
-    image: '/images/moon.png',
-    description:
-                'Fly me to the Moon',
+    image: 'https://i.imgur.com/DrTY6E1.png',
+    description: 'Fly me to the Moon',
     company: 'Apple',
     category: 'Moon',
     price: 10000,
@@ -29,8 +27,7 @@ const products = [
     _id: '3',
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.jpeg',
-    description:
-                'the Canon EOS 80D further clarifies itself using a pair of ',
+    description: 'the Canon EOS 80D further clarifies itself using a pair of ',
     company: 'Cannon',
     category: 'Electronics',
     price: 929.99,
@@ -43,7 +40,7 @@ const products = [
     name: 'Sony Playstation 5 Pro White Version',
     image: '/images/playstation.jpg',
     description:
-                'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+        'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     company: 'Sony',
     category: 'Electronics',
     price: 399.99,
@@ -56,7 +53,7 @@ const products = [
     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
-                'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+        'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     company: 'Logitech',
     category: 'Electronics',
     price: 49.99,
@@ -69,7 +66,7 @@ const products = [
     name: 'Amazon Echo Dot 3rd Generation',
     image: '/images/alexa.jpg',
     description:
-                'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+        'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
     company: 'Amazon',
     category: 'Electronics',
     price: 29.99,
